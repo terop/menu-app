35d9780200dfad2b07363614ee9deaeb5163744b
Tero Paloheimo <tero.paloheimo@iki.fi>
2016-04-14 16:51:16 +0300

    Add column for storing adding time to menus table

--- db-def.sql
+++ db-def.sql
@@ -2,6 +2,7 @@
 -- Table for storing menus for each week
 CREATE TABLE menus (
        id SERIAL PRIMARY KEY,
+       added_at TIMESTAMP NOT NULL DEFAULT now(),
        start_date DATE NOT NULL,
        end_date DATE NOT NULL,
        menu JSONB NOT NULL
