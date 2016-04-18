3ebbfa5dc73e55221b7f509508b1d6661e15117e
Tero Paloheimo <tero.paloheimo@iki.fi>
2016-04-18 08:23:41 +0300

    Move some files to a new directory and create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module setup.py."""
+
+from distutils.core import setup
+
+setup(
+    name='menu-app',
+    version='0.1',
+    description='Application for listing lunch restaurant menus',
+    author='Tero Paloheimo',
+    packages=['menu', 'test'],
+)
